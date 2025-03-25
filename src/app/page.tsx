28708ab82a5676d14f0a2caa462a51bf6879b7bb
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-03-25 16:30:15 +0600

    Configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
 export default function Home() {
-  return (
-    <div className="container text-[150px] bg-secondary font-rubik">Main page
-    <div className="text-[150px] font-rubik">Main page
-    </div>
-    <div className="text-[150px] font-sans">Main page
-    </div>
-    </div>
-  );
+  return <div className='h1'>HOME PAGE
+  </div>;
 }
