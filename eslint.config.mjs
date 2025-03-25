a5b95418110a89f3f88370ed6395bd1a1e497032
Syimyk <syjmykbejsekeev572@gmail.com>
2025-03-25 17:04:33 +0600

    create config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
+import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+	...compat.config({
+		extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+		plugins: ['prettier'],
+		rules: {
+			'prettier/prettier': 'error',
+			'arrow-body-style': ['error', 'always'],
+			'prefer-arrow-callback': 'error',
+			semi: ['error'],
+			quotes: ['error', 'single'],
+			'prefer-template': 'error',
+			'func-style': ['error', 'expression'],
+			indent: ['error', 'tab'],
+			'jsx-quotes': ['error', 'prefer-single'],
+		},
+		ignorePatterns: ['node_modules/', 'build/', 'dist/'],
+	}),
 ];
 
 export default eslintConfig;
