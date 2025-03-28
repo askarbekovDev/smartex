2fc3daf39c6971d0b3a4e50b912d9bf3576ed6db
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-03-28 16:37:28 +0600

    burger menu is in process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { IntroBlock } from '@/modules';
 
 export default function Home() {
 	return (
-		<div className='container'>
+		<div className=''>
 			<IntroBlock />
 		</div>
 	);
--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -1,15 +1,33 @@
 import React, { FC } from 'react';
 import { Burger } from '../../public/icons';
+import { headerNavLinks } from '@/utils/constants';
+import Link from 'next/link';
 
 export const BurgerMenu: FC = () => {
 	return (
-		<div className='hidden group relative w1150:block cursor-pointer hover:bg-red-400'>
+		<div className='hidden group relative w1150:block cursor-pointer'>
 			<Burger />
 			<div
-				className='w-10 h-10 absolute left-[-10px] pointer-events-none top-[39px] bg-white shadow-xl rounded-b-2xl opacity-0 translate-y-[-10px] 
-            transition-all duration-300 group-hover:pointer-events-auto group-hover:opacity-100 group-hover:translate-y-0 px-4 py-[10px]'
+				className='absolute w-[350px] px-14 pt-10 gap-3 z-10 flex flex-col whitespace-nowrap right-0 pointer-events-none top-[-28px]
+				 bg-white shadow-xl rounded-b-2xl opacity-0 translate-y-[-10px] transition-all duration-300 
+						group-hover:pointer-events-auto group-hover:opacity-100 group-hover:translate-y-0'
 			>
-				dv
+				{headerNavLinks.map((navLink, idx) => (
+					<Link key={idx} href={navLink.href}>
+						{navLink.title}
+					</Link>
+				))}
+				<span className='flex flex-col gap-4 uiBtnText'>
+					<Link className='w-full text-center text-white py-3 rounded-lg bg-success mt-2' href='#'>
+						Войти
+					</Link>
+					<Link
+						className='w-full text-center py-3 rounded-lg border-[1px] border-success mb-32'
+						href='#'
+					>
+						Регистрация
+					</Link>
+				</span>
 			</div>
 		</div>
 	);
