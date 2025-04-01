f6f1f084c89373c4fd288f3be70624fea0417a04
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-04-01 19:18:32 +0600

    backdrop is in process

--- src/components/ContactUsPopUp.tsx
+++ src/components/ContactUsPopUp.tsx
@@ -14,21 +14,21 @@ export const ContactUsPopUp: FC = () => {
 			<div
 				onClick={() => setFeedbackOpen(false)}
 				className={clsx(
-					'absolute inset-0 bg-black/40 transition-all duration-300 z-30',
+					'fixed inset-0 bg-black/40 transition-all duration-300 z-30',
 					{
 						'opacity-100 pointer-events-auto': feedbackOpen,
 						'opacity-0 pointer-events-none': !feedbackOpen,
 					}
 				)}
 			>
-				<div
+			</div>
+      <div
 					onClick={(e) => e.stopPropagation()}
-					className={clsx('ml-20 mt-20 w-[588px] h-[726px] bg-white transition-all duration-300', {
+					className={clsx('absolute ml-20 mt-20 w-[588px] h-[726px] bg-white transition-all duration-300', {
 						'scale-100': feedbackOpen,
-						'scale-50': !feedbackOpen,
+						'scale-50 hidden': !feedbackOpen,
 					})}
 				></div>
-			</div>
 		</>
 	);
 };
