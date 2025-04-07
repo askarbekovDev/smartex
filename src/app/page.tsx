b01e6ad923dd2c28c51810c7735dabd3fd232356
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-04-07 16:08:04 +0600

    Search block in process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { IntroBlock } from '@/modules';
+import { IntroBlock, SearchSection } from '@/modules';
 
 export default function Home() {
 	return (
 		<>
 			<IntroBlock />
+			<SearchSection />
+			<br /><br /><br /><br /><br /><br /><br /><br />
 		</>
 	);
 }
