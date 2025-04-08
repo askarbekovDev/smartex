7060735f09c936f59fa1cc7f34e4dbb076a9672b
bekturb <bekkgboy2@gmail.com>
2025-04-08 12:59:50 +0600

    started(our-work)/started page

---
+++ src/app/our-work/page.tsx
@@ -0,0 +1,12 @@
+import { WorkInfo } from '@/modules';
+import React from 'react';
+
+const OurWork = () => {
+	return (
+		<>
+			<WorkInfo />
+		</>
+	);
+};
+
+export default OurWork;
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,3 +1,4 @@
 export { IntroBlock } from './intro';
 export { SearchSection } from './search-modules';
 export { PinduoduoBlock } from './pinduoduo-block'
+export { WorkInfo } from './work-info';
