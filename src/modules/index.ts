a50f987e4e9a91423313262f10ccf2a6f617446f
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-04-08 16:37:58 +0600

    The main page is almost ready

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,8 @@
-export { NewsBlock } from "./news-block";
-export { NewsDetail } from "./news-detail";
-export { NewsRoom } from "./news-room";
-export { NewsIntro } from "./news-intro";
+export { NewsBlock } from './news-block';
+export { NewsDetail } from './news-detail';
+export { NewsRoom } from './news-room';
+export { NewsIntro } from './news-intro';
 export { IntroBlock } from './intro';
 export { SearchSection } from './search-modules';
-export { PinduoduoBlock } from './pinduoduo-block'
+export { PinduoduoBlock } from './pinduoduo-block';
+export { PopularQuestionsBlock } from './popular-questions-block';
