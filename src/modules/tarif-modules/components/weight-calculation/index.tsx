668aba9265e7a3a72997ffe6dc52be71f6abd8c6
Syimyk <syjmykbejsekeev572@gmail.com>
2025-04-11 21:39:50 +0600

    save

---
+++ src/modules/tarif-modules/components/weight-calculation/index.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import Image from 'next/image';
+import Korobka from '../../../../../public/images/korobka.png';
+export const WeightCalculation = () => {
+	return (
+		<section className='w-full flex gap-6 w650:flex-col mt-[40px]'>
+			<div className='w-full max-w-[270px] h-[150px] self-center border border-border rounded-[16px] p-3'>
+				<Image src={Korobka} alt='Korobka' className='w-full h-full object-contain' />
+			</div>
+			<div className={clsx('self-end')}>
+				<p className='h6'>Фактический вес </p>
+				<p className='bodyText mt-3'>
+					– это реальный физический вес груза, измеряемый в килограммах. Он учитывается при расчетах
+					стоимости перевозки и подборе подходящего транспорта. Для измерения используются
+					стандартные весы, а данные вносятся в транспортные документы. Этот показатель особенно
+					важен для тяжелых грузов с небольшими габаритами.
+				</p>
+			</div>
+		</section>
+	);
+};
