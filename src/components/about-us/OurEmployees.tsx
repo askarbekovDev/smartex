046bc265d62156af8e78744619ad9358a30dcc96
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-04-11 22:33:27 +0600

    Smartex team section is decomposited

---
+++ src/components/about-us/OurEmployees.tsx
@@ -0,0 +1,33 @@
+import { TeamMemberCard } from '@/UI';
+import clsx from 'clsx';
+import React, { FC } from 'react';
+
+type OurEmployeesProps = {
+	content: Content;
+};
+
+export const OurEmployees: FC<OurEmployeesProps> = ({ content }) => {
+	const teamMember = {
+		fullname: 'Жандарбек Джумаев',
+		position: 'Менеджер',
+		image: '/images/team-member.png',
+	};
+	return (
+		<div className={clsx('flex justify-center', { hidden: content !== 'Сотрудники' })}>
+			<div className='w-fit grid grid-cols-3 gap-6 justify-center w1150:grid-cols-2 w550:grid-cols-1'>
+				{Array(6)
+					.fill(0)
+					.map((el, idx) => (
+						<div className='w550:odd:hidden'>
+							<TeamMemberCard
+								key={idx}
+								fullname={teamMember.fullname}
+								position={teamMember.position}
+								image={teamMember.image}
+							/>
+						</div>
+					))}
+			</div>
+		</div>
+	);
+};
--- src/modules/smartex-team/index.tsx
+++ src/modules/smartex-team/index.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import { Pagination, TeamMemberCard, ToggleButton } from '@/UI';
+import { ToggleButton } from '@/UI';
 import {
 	employeesContent,
 	smartexTeamContent,
 	socialResponsibilityContent,
 } from '@/utils/constants';
-import Image from 'next/image';
 import React, { FC, useState } from 'react';
-import { CornerSmoother } from '../../../public/icons';
-import clsx from 'clsx';
-
-type Content = 'Наша команда' | 'Сотрудники' | 'Социальная ответственность';
+import { OurEmployees, OurTeam, SocialResponsibility } from '@/components/about-us';
 
 export const SmartexTeam: FC = () => {
 	const [content, setContent] = useState<Content>('Наша команда');
@@ -30,12 +26,6 @@ export const SmartexTeam: FC = () => {
 			? employeesContent
 			: socialResponsibilityContent;
 
-	const teamMember = {
-		fullname: 'Жандарбек Джумаев',
-		position: 'Менеджер',
-		image: '/images/team-member.png',
-	};
-
 	return (
 		<div className='container'>
 			<div className='flex justify-between pt-[100px] w1050:pt-15 w1050:flex-col-reverse w850:pt-20'>
@@ -49,96 +39,9 @@ export const SmartexTeam: FC = () => {
 			<p className='bodyText pt-4 max-w-[558px] w-full'>{contentToAppear}</p>
 
 			<div className='pt-17 w1150:pt-10 w850:pt-6'>
-				<div
-					className={clsx('transition-all duration-500', {
-						'scale-0 opacity-0 absolute pointer-events-none': content !== 'Наша команда',
-						'scale-100 opacity-100 pointer-events-auto': content === 'Наша команда',
-					})}
-				>
-					<div
-						className='h-[700px] rounded-[15px] overflow-hidden flex items-center justify-center w1050:h-[550px] w950:h-[500px] 
-          w850:h-[440px] w750:h-[380px] w650:h-[330px] w550:h-[290px] w450:h-[260px] w370:h-[240px]'
-					>
-						<Image
-							alt='Our team'
-							src='/images/our-team.jpeg'
-							width={1140}
-							height={600}
-							className='h-full w-full object-cover'
-						/>
-					</div>
-					<div className='w-full relative'>
-						<div
-							className='absolute w-fit right-[-1px] bottom-[-1px] pt-4 px-4 pb-px rounded-t-[40px] bg-background
-            left-1/2 -translate-x-1/2 w950:rounded-t-4xl w950:pt-3 w750:pt-2'
-						>
-							<Pagination />
-							<div className='absolute left-[-17px] bottom-px w-[17px] h-[17px] rotate-180'>
-								<CornerSmoother />
-							</div>
-							<div className='absolute bottom-px right-[-17px] w-[17px] h-[17px] rotate-270'>
-								<CornerSmoother />
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div
-					className={clsx('flex justify-center transition-all duration-500', {
-						'scale-0 opacity-0 absolute pointer-events-none': content !== 'Сотрудники',
-						'scale-100 opacity-100 pointer-events-auto': content === 'Сотрудники',
-					})}
-				>
-					<div className='w-fit grid grid-cols-3 gap-6 justify-center w1150:grid-cols-2 w550:grid-cols-1'>
-						{Array(6)
-							.fill(0)
-							.map((el, idx) => (
-								<div className='w550:odd:hidden'>
-									<TeamMemberCard
-										key={idx}
-										fullname={teamMember.fullname}
-										position={teamMember.position}
-										image={teamMember.image}
-									/>
-								</div>
-							))}
-					</div>
-				</div>
-
-				<div
-					className={clsx('transition-all duration-500', {
-						'scale-0 opacity-0 absolute pointer-events-none':
-							content !== 'Социальная ответственность',
-						'scale-100 opacity-100 pointer-events-auto': content === 'Социальная ответственность',
-					})}
-				>
-					<div
-						className='h-[700px] rounded-[15px] overflow-hidden flex items-center justify-center w1050:h-[550px] w950:h-[500px] 
-          w850:h-[440px] w750:h-[380px] w650:h-[330px] w550:h-[290px] w450:h-[260px] w370:h-[240px]'
-					>
-						<Image
-							alt='Support And Care'
-							src='/images/support-and-care.png'
-							width={1140}
-							height={600}
-							className='h-full w-full object-cover'
-						/>
-					</div>
-					<div className='w-full relative'>
-						<div
-							className='absolute w-fit right-[-1px] bottom-[-1px] pt-4 px-4 pb-px rounded-t-[40px] bg-background
-            left-1/2 -translate-x-1/2 w950:rounded-t-4xl w950:pt-3 w750:pt-2'
-						>
-							<Pagination />
-							<div className='absolute left-[-17px] bottom-px w-[17px] h-[17px] rotate-180'>
-								<CornerSmoother />
-							</div>
-							<div className='absolute bottom-px right-[-17px] w-[17px] h-[17px] rotate-270'>
-								<CornerSmoother />
-							</div>
-						</div>
-					</div>
-				</div>
+				<OurTeam content={content} />
+				<OurEmployees content={content} />
+				<SocialResponsibility content={content} />
 			</div>
 		</div>
 	);
