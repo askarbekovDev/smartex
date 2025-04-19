cd6a76ec22f6c70c0d24014acd55574521201d96
Syimyk <syjmykbejsekeev572@gmail.com>
2025-04-19 20:40:24 +0600

    save

---
+++ src/app/shops/page.tsx
@@ -0,0 +1,11 @@
+import { SmartexShops } from '@/modules';
+
+const Shops = () => {
+	return (
+		<div>
+			<SmartexShops />
+		</div>
+	);
+};
+
+export default Shops;
---
+++ src/modules/shops-modules/index.tsx
@@ -0,0 +1,9 @@
+import { FAQ } from '../main-faq-modules';
+
+export const SmartexShops = () => {
+	return (
+		<div className='container'>
+			<FAQ variant='orange' />
+		</div>
+	);
+};
