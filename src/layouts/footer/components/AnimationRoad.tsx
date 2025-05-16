0f0437a8748a3582ea1a318933d749561f2797d2
bekturb <bekkgboy2@gmail.com>
2025-05-16 17:01:02 +0600

    changed(footer)/responsible airplane

--- src/layouts/footer/components/AnimationRoad.tsx
+++ src/layouts/footer/components/AnimationRoad.tsx
@@ -6,25 +6,28 @@ import Line from '../../../../public/images/line.png';
 
 export const AnimationRoad = () => {
 	return (
-		<div className='relative w-full mx-auto h-[100px] rounded-t-[1rem] overflow-hidden'>
-			<Image
-				src={Line}
-				alt='Road'
-				className='absolute w-full h-full left-1/2 -translate-y-1/2 top-1/2 transform -translate-x-1/2 pointer-events-none'
-				draggable={false}
-			/>
+		<div className='relative overflow-x-hidden h-[120px]'>
+			<div className='absolute bottom-0 w-full h-[100px] overflow-hidden'>
+				<Image
+					src={Line}
+					alt='Road'
+					className='absolute w-full h-full left-1/2 -translate-y-1/2 top-1/2 transform -translate-x-1/2 pointer-events-none'
+					draggable={false}
+				/>
+			</div>
+
 			<motion.div
 				initial={{ x: '-10%' }}
 				animate={{ x: '100%' }}
 				transition={{ repeat: Infinity, duration: 20, ease: 'linear' }}
-				className='absolute top-0 z-1 inset-0 left-0 w-full h-auto'
+				className='absolute top-1/2 transform -translate-y-1/2 left-0 w-full z-10 pointer-events-none'
 			>
 				<Image
 					src={AirPlane}
 					alt='Car'
 					width={250}
 					height={150}
-					className='drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
+					className='drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] w-[340px] w550:w-[250px]'
 				/>
 			</motion.div>
 		</div>
