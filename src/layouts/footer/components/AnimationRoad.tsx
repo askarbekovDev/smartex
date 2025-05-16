70c584ca59321b5f9759bdb80e5256f9d7d3ab52
bekturb <bekkgboy2@gmail.com>
2025-05-16 15:02:29 +0600

    changed(AnimationRoad icons and road)/refactored

--- src/layouts/footer/components/AnimationRoad.tsx
+++ src/layouts/footer/components/AnimationRoad.tsx
@@ -1,14 +1,14 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import React from 'react';
-import CarIcon from '../../../../public/icons/car-icon.svg';
-import Road from '../../../../public/icons/road.svg';
+import AirPlane from '../../../../public/images/air-plane.png';
+import Line from '../../../../public/images/line.png';
 
 export const AnimationRoad = () => {
 	return (
-		<div className='relative w-full mx-auto h-[100px] bg-primary rounded-t-[1rem] overflow-hidden'>
+		<div className='relative w-full mx-auto h-[100px] rounded-t-[1rem] overflow-hidden'>
 			<Image
-				src={Road}
+				src={Line}
 				alt='Road'
 				className='absolute w-full h-full left-1/2 -translate-y-1/2 top-1/2 transform -translate-x-1/2 pointer-events-none'
 				draggable={false}
@@ -17,9 +17,15 @@ export const AnimationRoad = () => {
 				initial={{ x: '-10%' }}
 				animate={{ x: '100%' }}
 				transition={{ repeat: Infinity, duration: 20, ease: 'linear' }}
-				className='absolute bottom-2 z-1 inset-0 left-0 w-full h-auto'
+				className='absolute top-1/2 transform -translate-y-1/2 z-1 inset-0 left-0 w-full h-auto'
 			>
-				<Image src={CarIcon} alt='Car' className='drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]' />
+				<Image
+					src={AirPlane}
+					alt='Car'
+					width={150}
+					height={150}
+					className='drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
+				/>
 			</motion.div>
 		</div>
 	);
