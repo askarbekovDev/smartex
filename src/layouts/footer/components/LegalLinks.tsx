4eac4df8afd0c6b78c5a727bfb7b1ad727294e19
zh33nbekov <rai.zheenbekov@gmail.com>
2025-05-24 16:25:48 +0600

    refactored(footer)/updated LegalLinks to use current year dynamically added(news-block)/created NewsBlock component for displaying news refactored(news-section)/simplified NewsSection header structure

--- src/layouts/footer/components/LegalLinks.tsx
+++ src/layouts/footer/components/LegalLinks.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-
 export const LegalLinks = () => {
+	const fullYear = new Date().getFullYear();
+
 	return (
 		<div className='w-full bg-secondary flex items-center py-4'>
 			<div className='container mx-auto px-4 flex flex-col lg:flex-row justify-between items-center text-primary text-sm gap-2'>
-				<p>© 2024 All rights reserved.</p>
+				<p>© {fullYear} All rights reserved.</p>
 				<div className='flex gap-6'>
 					<p className='cursor-pointer'>Terms of Service</p>
 					<p className='cursor-pointer'>Privacy Policy</p>
