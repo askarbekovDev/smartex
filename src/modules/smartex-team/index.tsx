fd759d4fdeeb7651ee09b9aec7f27a8565bd9da5
Bekbol Askarbekov <e-mail-post@mail.ru>
2025-05-29 12:38:33 +0600

    ..

--- src/modules/smartex-team/index.tsx
+++ src/modules/smartex-team/index.tsx
@@ -25,7 +25,6 @@ export const SmartexTeam: FC = () => {
 			: content === 'Сотрудники'
 			? employeesContent
 			: socialResponsibilityContent;
-			: socialResponsibilityContent;
 
 	return (
 		<section className='container'>
